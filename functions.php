21afcf280edc9cd12779bb1a3cd5ae099a641864
oki2a24 <oki2a24@gmail.com>
2019-02-23 17:03:30 +0900

    feat: 親テーマのスタイルシートをキューに入れる

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * JavaScript や CSS を読み込みます。
+ */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
