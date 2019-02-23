41d1251aadfdeb0bed5cf33a2528fbcadf31b23c
oki2a24 <oki2a24@gmail.com>
2019-02-23 17:09:48 +0900

    style: インデントを 2 スペースに変更する

--- functions.php
+++ functions.php
@@ -4,14 +4,14 @@
  */
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
 /**
  * more タグで URL 末端に付く #more-xxxx を削除します。
  */
 function remove_more_link_scroll( $link ) {
-    $link = preg_replace( '|#more-[0-9]+|', '', $link );
-    return $link;
+  $link = preg_replace( '|#more-[0-9]+|', '', $link );
+  return $link;
 }
 add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
