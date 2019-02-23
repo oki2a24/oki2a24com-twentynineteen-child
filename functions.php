575b4d159280b70457fd935bcb76c5a526b4c7c5
oki2a24 <oki2a24@gmail.com>
2019-02-23 17:08:14 +0900

    feat: more タグによる URL 追加を無効にする

--- functions.php
+++ functions.php
@@ -6,3 +6,12 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+/**
+ * more タグで URL 末端に付く #more-xxxx を削除します。
+ */
+function remove_more_link_scroll( $link ) {
+    $link = preg_replace( '|#more-[0-9]+|', '', $link );
+    return $link;
+}
+add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
