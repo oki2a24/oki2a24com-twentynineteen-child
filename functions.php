322996b198d70603a97afb723e15cffeb52cd8c3
oki2a24 <oki2a24@gmail.com>
2019-03-11 08:20:31 +0900

    feat: touch-keyboard-navigation.js を子テーマで読み込む

--- functions.php
+++ functions.php
@@ -5,7 +5,25 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_enqueue_script(
+    'twentynineteen-child-touch-navigation',
+    get_stylesheet_directory_uri() . '/js/touch-keyboard-navigation.js',
+    array('twentynineteen-priority-menu'),
+    false,
+    true
+  );
 }
+
+/**
+ * 親テーマの JavaScript ファイルをデキューします。
+ *
+ * Hooked to the wp_print_scripts action, with a late priority (100),
+ * so that it is after the script was enqueued.
+ */
+function wpdocs_dequeue_script() {
+   wp_dequeue_script( 'twentynineteen-touch-navigation' );
+}
+add_action( 'wp_print_scripts', 'wpdocs_dequeue_script', 100 );
 
 /**
  * more タグで URL 末端に付く #more-xxxx を削除します。
