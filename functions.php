b084efdf0fecfaf5500ef1c7eef97d4e2d37c8fc
oki2a24 <oki2a24@gmail.com>
2019-03-12 08:24:02 +0900

    rafactor: 子テーマでの不要なカスタマイザー設定を削除する

--- functions.php
+++ functions.php
@@ -54,75 +54,6 @@ add_filter( 'wp_nav_menu_items', 'add_search_box_to_menu', 10, 2);
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function child_twentynineteen_customize_register( $wp_customize ) {
-  $wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-  $wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-  $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-
-  if ( isset( $wp_customize->selective_refresh ) ) {
-    $wp_customize->selective_refresh->add_partial(
-      'blogname',
-      array(
-        'selector'        => '.site-title a',
-        'render_callback' => 'twentynineteen_customize_partial_blogname',
-      )
-    );
-    $wp_customize->selective_refresh->add_partial(
-      'blogdescription',
-      array(
-        'selector'        => '.site-description',
-        'render_callback' => 'twentynineteen_customize_partial_blogdescription',
-      )
-    );
-  }
-
-  /**
-   * Primary color.
-   */
-  $wp_customize->add_setting(
-    'primary_color',
-    array(
-      'default'           => 'default',
-      'transport'         => 'postMessage',
-      'sanitize_callback' => 'twentynineteen_sanitize_color_option',
-    )
-  );
-
-  $wp_customize->add_control(
-    'primary_color',
-    array(
-      'type'     => 'radio',
-      'label'    => __( 'Primary Color', 'twentynineteen' ),
-      'choices'  => array(
-        'default' => _x( 'Default', 'primary color', 'twentynineteen' ),
-        'custom'  => _x( 'Custom', 'primary color', 'twentynineteen' ),
-      ),
-      'section'  => 'colors',
-      'priority' => 5,
-    )
-  );
-
-  // Add primary color hue setting and control.
-  $wp_customize->add_setting(
-    'primary_color_hue',
-    array(
-      'default'           => twentynineteen_get_default_hue(),
-      'transport'         => 'postMessage',
-      'sanitize_callback' => 'absint',
-    )
-  );
-
-  $wp_customize->add_control(
-    new WP_Customize_Color_Control(
-      $wp_customize,
-      'primary_color_hue',
-      array(
-        'description' => __( 'Apply a custom color for buttons, links, featured images, etc.', 'twentynineteen' ),
-        'section'     => 'colors',
-        'mode'        => 'hue',
-      )
-    )
-  );
-
   // Add image filter setting and control.
   $wp_customize->add_setting(
     'image_filter',
@@ -130,15 +61,6 @@ function child_twentynineteen_customize_register( $wp_customize ) {
       'default'           => 0,
       'sanitize_callback' => 'absint',
       'transport'         => 'postMessage',
-    )
-  );
-
-  $wp_customize->add_control(
-    'image_filter',
-    array(
-      'label'   => __( 'Apply a filter to featured images using the primary color', 'twentynineteen' ),
-      'section' => 'colors',
-      'type'    => 'checkbox',
     )
   );
 }
